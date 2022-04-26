32ebf8b7a4f98d66dd82593af50318aa95cfd266
krishna <krishna.1923en1078@kiet.edu>
2022-04-26 13:35:54 +0530

    a few changes while learning new concepts

--- src/main/java/com/in28minutes/springboot/WelcomeController.java
+++ src/main/java/com/in28minutes/springboot/WelcomeController.java
@@ -1,14 +1,21 @@
 package com.in28minutes.springboot;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.in28minutes.service.WelcomeService;
+import com.in28minutes.springboot.WelcomeService;
+import com.in28minutes.springboot.configuration.BasicConfiguration;
 
 @RestController
 public class WelcomeController {
+	
+	@Autowired
+	private BasicConfiguration configuration;
 	
 	@Autowired
 	private WelcomeService service;
@@ -16,7 +23,17 @@ public class WelcomeController {
 	@RequestMapping("/welcome")
 	public String welcomePage()
 	{
-		return "Welcome"+service.retrieveWelcomeMessage();
+		return service.retrieveWelcomeMessage();
+	}
+	
+	@RequestMapping("/dynamic-configuration")
+	public Map dynamicConfiguration()
+	{
+		Map map=new HashMap();
+		map.put("message",configuration.getMessage());
+		map.put("number",configuration.getNumber());
+		map.put("value",configuration.isValue());
+		return map;
 	}
 
 }
